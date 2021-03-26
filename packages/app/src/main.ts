aae36e901e455229211d7f14bc82e6c660fa4b9e
SegaraRai <SegaraRai@users.noreply.github.com>
2021-03-26 23:40:55 +0900

    update

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/main.css';
+import { fetchUserId } from './lib/userAccount';
 
-createApp(App)
-  .use(router)
-  .mount('#app');
+createApp(App).use(router).mount('#app');
+
+fetchUserId();
